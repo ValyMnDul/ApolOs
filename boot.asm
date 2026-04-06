09dca0b5c420239db38e3ef5183f1d6f20da5197
ValyMnDul <mindrilavasilevalentin@gmail.com>
2026-04-06 10:02:14 +0300

    init; add boot.asm (bootloader)

---
+++ boot.asm
@@ -0,0 +1,26 @@
+[org 0x7c00]
+
+mov ah, 0x0e
+
+mov al, 'H'
+int 0x10
+
+mov al, 'i'
+int 0x10
+
+mov al, 0x0a
+int 0x10
+
+mov al, 0x0d
+int 0x10
+
+mov al, 'H'
+int 0x10
+
+mov al, 'C'
+int 0x10
+
+jmp $
+
+times 510-($-$$) db 0
+dw 0xaa55
