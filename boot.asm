d0f736b70fb4cdfd4b8fbad987095f66d841e363
ValyMnDul <mindrilavasilevalentin@gmail.com>
2026-04-06 17:43:02 +0300

    update GDT definitions; Real mode to protected mode!

--- boot.asm
+++ boot.asm
@@ -21,9 +21,42 @@ init_pm:
     mov fs, ax
     mov gs, ax
 
-    mov ebp 0x90000
+    mov ebp, 0x90000
     mov esp, ebp
-    
+
+    mov ebx, 0xb8000
+    mov byte [ebx], 'A'
+    mov byte [ebx+1], 0x0A
+
+    jmp $
+
+gdt_start:
+
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+gdt_code:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
     
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
