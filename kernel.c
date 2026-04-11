fe1432c694bda5ffeaf8eea5ce06bedf36ce8bca
ValyMnDul <mindrilavasilevalentin@gmail.com>
2026-04-11 20:31:22 +0300

    Add IDT gate setup and PIC initialization

--- kernel.c
+++ kernel.c
@@ -227,6 +227,30 @@ void printf(char* format, ...){
     }
 }
 
+void idtSetGate(int nr, unsigned int adr){
+    idt[nr].offset_low = adr & 0xFFFF;
+    idt[nr].selector = 0x08;
+    idt[nr].zero = 0;
+    idt[nr].type_attr = 0x8E;
+    idt[nr].offset_high = (adr >> 16) & 0xFFFF;
+}
+
+void initPic(){
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
+}
+
 void keyboard_handler(){
     unsigned char scancode = inb(0x60);
     printf("\n%d", scancode);
