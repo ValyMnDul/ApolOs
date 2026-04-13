64e3b32dbababcc547dc6f9098829e2a8543caba
ValyMnDul <mindrilavasilevalentin@gmail.com>
2026-04-13 11:05:35 +0300

    Add QWERTY key mapping to keyboard handler for character output

--- kernel.c
+++ kernel.c
@@ -277,7 +277,20 @@ void idtInstall(){
 }
 
 __attribute__((interrupt)) void keyboard_handler(struct interrupt_frame* frame){
+
+    unsigned char QWERTY[128] = {
+        0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+        '-', '=', '\b', '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u',
+        'i', 'o', 'p', '[', ']', '\n', 0, 'a', 's', 'd', 'f', 'g',
+        'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c',
+        'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0
+    };
+
     unsigned char scancode = inb(0x60);
-    printf("\n%d", scancode);
+    
+    if(scancode < 128 && QWERTY[scancode] != 0){
+        printf("%c", QWERTY[scancode]);
+    }
+    
     outb(0x20, 0x20);
 }
