ad473781a339604aff0884d279fa4e141c448b38
ValyMnDul <mindrilavasilevalentin@gmail.com>
2026-05-05 11:50:02 +0300

    Reorgonize the title and add a test img

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 
@@ -6,10 +8,21 @@ export default function Main(){
     <>
       <Navbar/>
       <div className="flex flex-col">
-        <div className="h-auto w-full bg-primary pl-[10%] pr-[10%] pt-10 pb-10">
-          <h1 className="text-[3.5rem] text-background m-0">ApolOS</h1>
-          <h2 className="text-xl text-background font-bold">Nasa&apos;s OS))</h2>
-          <p className="text-[1rem] text-background">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla harum accusamus hic eaque, architecto, optio perferendis blanditiis, quis quibusdam doloribus minus ea veniam. Illum eius temporibus cumque doloribus obcaecati beatae!</p>
+        <div className="h-auto w-full bg-secondary pl-[10%] pr-[10%] pt-10 pb-10 flex flex-row justify-between flex-wrap">
+          <div className="w-[60%]">
+            <h1 className="text-[3.5rem] text-background m-0">ApolOS</h1>
+            <h2 className="text-xl text-background font-bold">Nasa&apos;s OS))</h2>
+            <p className="text-[1rem] text-background">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla harum accusamus hic eaque, architecto, optio perferendis blanditiis, quis quibusdam doloribus minus ea veniam. Illum eius temporibus cumque doloribus obcaecati beatae!</p>
+          </div>
+          <div className="w-[35%]">
+            <Image
+            className="w-auto h-auto"
+            alt="ApolOS"
+            src="/test.png"
+            width={300}
+            height={300}
+            />
+          </div>
         </div>
         <div className="w-full h-auto pl-[10%] pr-[10%] pt-10 pb-10">
           <section>
