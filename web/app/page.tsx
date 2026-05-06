20354a237e1a60e031524d4d12af4d3b5c876e7d
ValyMnDul <mindrilavasilevalentin@gmail.com>
2026-05-06 17:17:57 +0300

    Refactor layout and improve styling in Main component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -8,19 +8,20 @@ export default function Main(){
     <>
       <Navbar/>
       <div className="flex flex-col">
-        <div className="h-auto w-full bg-secondary pl-[10%] pr-[10%] pt-10 pb-10 flex flex-row justify-between flex-wrap">
-          <div className="w-[60%]">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+          <div className="w-full md:w-[60%]">
             <h1 className="text-[3.5rem] text-background m-0">ApolOS</h1>
-            <h2 className="text-xl text-background font-bold">Nasa&apos;s OS))</h2>
-            <p className="text-[1rem] text-background">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla harum accusamus hic eaque, architecto, optio perferendis blanditiis, quis quibusdam doloribus minus ea veniam. Illum eius temporibus cumque doloribus obcaecati beatae!</p>
+            <h2 className="text-xl text-background font-bold">Nasa&apos;s OS</h2>
+            <p className="text-lg text-background/90 max-w-xl leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla harum accusamus hic eaque, architecto, optio perferendis blanditiis, quis quibusdam doloribus minus ea veniam. Illum eius temporibus cumque doloribus obcaecati beatae!</p>
           </div>
-          <div className="w-[35%]">
-            <Image
-            className="w-auto h-auto"
-            alt="ApolOS"
-            src="/test.png"
-            width={300}
-            height={300}
+          <div className="w-full md:w-[35%]">
+            <Image 
+              src="/test.png" 
+              width={300} 
+              height={300} 
+              alt="ApolOS Interface"
+              priority
+              className="w-full h-auto object-contain" 
             />
           </div>
         </div>
